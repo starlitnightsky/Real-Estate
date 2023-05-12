f11bee5b4e2e946e6e30865c6a5e7aec617f046c
Harald Anderson <danddycandy@gmail.com>
2023-05-12 22:32:57 +0900

    feat: updated datatable

--- src/components/DataTable.tsx
+++ src/components/DataTable.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { AiFillEdit, AiFillDelete } from 'react-icons/ai'
 export interface DataTableProps {
   columns: string[]
   data: any
@@ -12,10 +12,7 @@ export const DataTable: React.FC<DataTableProps> = ({ columns, data }) => {
         <thead className='bg-[rgb(33,35,37)] text-xs uppercase font-medium text-center'>
           <tr>
             {columns.map((column) => (
-              <th
-                scope='col'
-                className='px-6 py-3 text-left tracking-wider text-center'
-              >
+              <th scope='col' className='px-6 py-3 tracking-wider text-center'>
                 {column}
               </th>
             ))}
@@ -26,9 +23,14 @@ export const DataTable: React.FC<DataTableProps> = ({ columns, data }) => {
             return (
               <tr className='bg-black bg-opacity-20'>
                 {columns.map((column) => {
-                  return (
+                  return column !== 'ACTION' ? (
                     <td className='px-6 py-4 whitespace-nowrap'>
                       {val[column]}
+                    </td>
+                  ) : (
+                    <td className='px-6 py-4 whitespace-nowrap flex flex-row justify-between mx-[10px]'>
+                      <AiFillEdit className=' text-green-800 cursor-pointer' />
+                      <AiFillDelete className='text-red-800 cursor-pointer' />
                     </td>
                   )
                 })}
